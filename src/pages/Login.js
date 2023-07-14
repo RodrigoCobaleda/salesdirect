8e38e81fe8fddb1b7186d0580a2120ea7f62e90e
Rodrigo picazo cobaleda <rodrigo.cobaleda.398@accesstomusic.ac.uk>
2023-07-14 11:38:20 +0100

    got rid of the google login
    was causing too many errors and didnt work

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -6,7 +6,7 @@
 
 import React from "react";
 import "../App.css";
-import GoogleLogin from "react-google-login";
+
 
 export default function Login() {
   const responseGoogle = (response) => {
@@ -19,14 +19,6 @@ export default function Login() {
       <h2>Google Login</h2>
       </div>
       <h3>{<br />}</h3>
-      <GoogleLogin
-        clientId="519846072436-hiroropn96squ7buljsig3u4ot1abmu2.apps.googleusercontent.com"
-        onSuccess={responseGoogle}
-        onFailure={responseGoogle}
-        cookiePolicy={"single_host_origin"}
-        theme="light"
-        Navigate="./Home"
-      />
       <h3>{<br />}</h3>
     </div>
   );
